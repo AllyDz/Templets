cb2fd8a7e8b52b11dac8ec26ed6488c7ed53ae09
Allyson Brusstar <AllysonBrusstar@gmail.com>
2016-09-12 21:46:12 -0400

    building JS

---
+++ FormTemplet/global.js
@@ -0,0 +1,21 @@
+alert('JS Loaded');
+// function showAnswer(){
+// 	document.getElementById("definition").innerHTML = "This is a test definition.";
+// }
+//var flashCardsId = "definition" + "term";
+//jQuery(document).ready(function(){
+// } card-
+	// jQuery(function){
+	// 	jQuery("btn").click(function(){
+	// 	jQuery ("definition").toggleClass("show");
+	// });
+
+//});
+
+//jQuery(btn).onClick(function(){ document.getElementById("definition").innerHTML = "This is a test defanition."});
+
+jQuery(function(){ 
+	jQuery('#btn').onClick(function(){
+		function('#definition').toggleClass('.show'); 
+	}; 
+});

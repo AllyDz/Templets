ab9c41fa352cfa25e010aeee0b1277a3472451d6
Allyson Brusstar <AllysonBrusstar@gmail.com>
2016-09-13 21:10:53 -0400

    editing js file

--- FormTemplet/global.js
+++ FormTemplet/global.js
@@ -1,21 +1,11 @@
-alert('JS Loaded');
-// function showAnswer(){
-// 	document.getElementById("definition").innerHTML = "This is a test definition.";
-// }
-//var flashCardsId = "definition" + "term";
-//jQuery(document).ready(function(){
-// } card-
-	// jQuery(function){
-	// 	jQuery("btn").click(function(){
-	// 	jQuery ("definition").toggleClass("show");
-	// });
+jQuery(document).ready(function(){
+	
+	alert('JS Loaded');
 
-//});
+	jQuery('#btn').click(function() {
+		jQuery('#definition').toggleClass('show'); 
+		('');
+	});
 
-//jQuery(btn).onClick(function(){ document.getElementById("definition").innerHTML = "This is a test defanition."});
-
-jQuery(function(){ 
-	jQuery('#btn').onClick(function(){
-		function('#definition').toggleClass('.show'); 
-	}; 
+	alert('JS Completed');
 });

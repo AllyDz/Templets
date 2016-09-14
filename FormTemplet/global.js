0b65e314a8752f809afa4a570c2393f4ab7b9286
Allyson Brusstar <AllysonBrusstar@gmail.com>
2016-09-14 13:17:31 -0400

    fixing js code, adding more to html

--- FormTemplet/global.js
+++ FormTemplet/global.js
@@ -1,11 +1,12 @@
+// @todo add the other id's that will be apart of the .toggleClass
 jQuery(document).ready(function(){
 	
-	alert('JS Loaded');
+	//alert('JS Loaded');
 
-	jQuery('#btn').click(function() {
-		jQuery('#definition').toggleClass('show'); 
-		('');
+	//button functions	
+	jQuery('#creat-deck, #pick-deck, #play, #answer').click(function() {
+		jQuery('#step2, #step3, #inplay, #definition').toggleClass('show');
 	});
 
-	alert('JS Completed');
+	//alert('JS Completed');
 });
